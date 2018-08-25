328b9c957bdb0dc7603e85e71406903476ba5f24
b23production GNU <b23prodtm@users.sourceforge.net>
2018-08-25 02:38:54 +0200

    Tests UI Bundles

---
+++ Xcode-iOS/SDL/Tests-tvOS/Tests_tvOS.m
@@ -0,0 +1,38 @@
+//
+//  Tests_tvOS.m
+//  Tests-tvOS
+//
+//  Created by www.b23prodtm.info on 25/08/2018.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Tests_tvOS : XCTestCase
+
+@end
+
+@implementation Tests_tvOS
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
